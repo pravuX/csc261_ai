9d7caebba16f29ffa2eb8ec6445cb98a7fdf2948
Prabesh Subedi <prabesh.sb@gmail.com>
2023-12-29 23:20:54 +0545

    Update variable names and remove unnecessary functions

--- bfs.py
+++ bfs.py
@@ -1,49 +1,44 @@
 from collections import deque
 
 graph = {
-    "A": ["B", "C"],
-    "B": ["D", "E"],
-    "C": ["F", "G"],
-    "D": [],
-    "E": [],
-    "F": [],
-    "G": [],
+    "S": ("A", "D"),
+    "A": ("B", "D", "S"),
+    "B": ("A", "C", "E"),
+    "C": ("B"),
+    "D": ("A", "S", "E"),
+    "E": ("D", "B", "F"),
+    "F": ("E", "G"),
+    "G": ("F"),
 }
-
-
-def is_destination(v, d):
-    if v == d:
-        return True
-    return False
-
-
-def show_path(source, visited_arr, destination):
-    path = ""
-    path += f"{source} --> "
-    for v in visited_arr:
-        path += f"{v} --> "
-    path += f"{destination}"
-    print(path);
+# graph = {
+#     "S": ("A", "G"),
+#     "A": ("B", "C"),
+#     "B": ("D"),
+#     "C": ("D", "G"),
+#     "D": ("G"),
+#     "G": (),
+# }
 
 
 def bfs(source, destination):
     # create the search queue
     search_queue = deque()
-    search_queue += graph[source]
+    search_queue.append(source)
     # list of expanded/ visited vertices
     visited = []
 
     while search_queue:
-        vertex = search_queue.popleft()
-        if not vertex in visited:
-            if is_destination(vertex, destination):
-                show_path(source, visited, destination)
+        node = search_queue.popleft()
+        if not node in visited:
+            visited.append(node)
+            if node == destination:
+                # show path
+                print(f"Path: {' --> '.join(visited)}")
                 return True
             else:
-                search_queue += graph[vertex]
-                visited.append(vertex)
+                search_queue += graph[node]
     return False
 
 
 if __name__ == "__main__":
-    bfs("A", "E")
+    bfs("S", "G")
